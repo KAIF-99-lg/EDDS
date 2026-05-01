bcc3a74f28eb35b21f5eed116d186c55dfa5fab1
KAIF-99-lg <k82102002@gmail.com>
2026-05-02 00:42:34 +0530

    fix: render port binding

--- backend/app.py
+++ backend/app.py
@@ -197,4 +197,5 @@ with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
-    app.run(debug=os.getenv("FLASK_DEBUG", "True") == "True", port=int(os.getenv("PORT", 5000)))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
